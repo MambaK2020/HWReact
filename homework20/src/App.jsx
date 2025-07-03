292bd577b2e0fd19bf7da3c4d5c40defedaba094
Oleksandr <nagirnyak24@gmail.com>
2025-07-03 21:14:12 +0200

    add homework20

---
+++ homework20/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react'
+import UserList from './redux/components/UserList'
+
+function App() {
+
+
+  return (
+    <div>
+      <UserList />
+    </div>
+  )
+}
+
+export default App
---
+++ homework20/src/redux/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+
+    list: [
+        {name: 'Alice', email: 'alice@example.com'},
+        {name: 'Bob', email: 'bob@example.com'},
+        {name: "Charley", email: 'charlie@example.com'}
+    ]
+};
+
+const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers:{}
+})
+
+export default userSlice.reducer
